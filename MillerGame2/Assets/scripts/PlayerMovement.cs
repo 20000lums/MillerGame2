db69690c24269b26d6458db8b7641d1037cc4341
20000lums <jm686sc@gmail.com>
2024-01-22 12:59:49 -0800

    things done input started

--- MillerGame2/Assets/scripts/PlayerMovement.cs
+++ MillerGame2/Assets/scripts/PlayerMovement.cs
@@ -11,7 +11,8 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask ground;
     private Vector2 moveVector;
     private ContactFilter2D groundFilter;
-    
+    private int state = 1;
+
     void Start()
     {
         groundFilter.useTriggers = false;
@@ -24,25 +25,24 @@ public class PlayerMovement : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.D))
         {
-            moveVector.x += 1;
+            moveVector.x += .1f;
         }
         if(Input.GetKey(KeyCode.A))
         {
-            moveVector.x += -1;
+            moveVector.x += -.1f;
         }
         if(Input.GetKey(KeyCode.W))
         {
-            moveVector.y += 1;
+            moveVector.y += .1f;
         }
         if(Input.GetKey(KeyCode.S))
         {
-            moveVector.y += -1;
+            moveVector.y += -.1f;
         }
-        RB.velocity = moveVector;
+        move(moveVector);
         moveVector = new Vector2(0, 0);
     }
-    //simulates normal force. moving using this method will prevent you from going through layer "ground" but will be uneffected by physics
-    
+    //simulates normal force. moving using this method will prevent you from going through layer "ground" but will be uneffected by momentum n stuff
     List<bool> move(Vector2 Direction) 
     {
         RaycastHit2D[] Results = new RaycastHit2D[16];
@@ -53,11 +53,11 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            RaycastHit2D cum;
-             List<RaycastHit2D> ResultsList = new List<RaycastHit2D>();
+            
+            List<RaycastHit2D> ResultsList = new List<RaycastHit2D>();
             for (int i = 0; i < Results.Length; i++)
             {
-                if(Vector2.Dot(Results[i].normal,Direction) > 0 )
+                if(Vector2.Dot(Results[i].normal,Direction) < 0 )
                 {
                     ResultsList.Add(Results[i]);
                 }
@@ -68,7 +68,7 @@ public class PlayerMovement : MonoBehaviour
                  RB.MovePosition(Direction + new Vector2(transform.position.x, transform.position.y));
                  return new List<bool>(){false, false};
             }
-            cum = ResultsList[0];
+            RaycastHit2D cum = ResultsList[0];
             for (int i = 1; i < ResultsList.Count; i++)
             {
                 if(cum.distance < ResultsList[i].distance)
@@ -78,8 +78,6 @@ public class PlayerMovement : MonoBehaviour
             }
             RB.MovePosition(cum.centroid);
             return new List<bool>(){cum.normal.x !=0, cum.normal.y !=0};
-
-
         }
     }
       
